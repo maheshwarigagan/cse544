4ff3ffe4917d69c0ea18a545863bf5aafced3bae
RonakKhandelwal <ronak9896@gmail.com>
2020-02-23 21:41:22 -0500

    Updating code and removing commented code.

--- a2_6.py
+++ a2_6.py
@@ -2,6 +2,7 @@ import numpy as np
 
 np.random.seed(960)
 
+
 def rand_walk(init_val, final_high, final_low, prob, N):
     total_low = 0
     total_high = 0
@@ -12,30 +13,28 @@ def rand_walk(init_val, final_high, final_low, prob, N):
         current = init_val
         steps = 0
         while current != final_high and current != final_low:
-            p = np.random.uniform(0,1)
-            if(p<prob):
+            p = np.random.uniform(0, 1)
+            if (p < prob):
                 current += 1
             else:
                 current -= 1
             steps += 1
 
         if current == final_high:
-            total_high+=1
+            total_high += 1
         else:
-            total_low+=1
+            total_low += 1
         total_expected += current
         total_steps += steps
 
-    a = total_high/N
-    b = total_low/N
-    c = total_expected/N
-    d = total_steps/N
-    return a,b,c,d
+    a = total_high / N
+    b = total_low / N
+    c = total_expected / N
+    d = total_steps / N
+    return a, b, c, d
+
 
 if __name__ == '__main__':
-    print(rand_walk(100,150,0,0.5,10000))
-    print(rand_walk(100,200,0,0.52,10000))
-    print(rand_walk(200,250,0,0.54,10000))
-    # Comment the following line, it's purely for testing purpose
-    print(rand_walk(100,150,0,0.49,10000))
-
+    print(rand_walk(100, 150, 0, 0.5, 10000))
+    print(rand_walk(100, 200, 0, 0.52, 10000))
+    print(rand_walk(200, 250, 0, 0.54, 10000))
--- a2_7.py
+++ a2_7.py
@@ -20,24 +20,3 @@ if __name__ == '__main__':
     Pr_s = np.array([[0.1], [0.2], [0.5], [0.9]])
     solution = np.matmul(steady_state, Pr_s)
     print("Numerical solution for part B as product of: \n",steady_state, " and \n", Pr_s, " Is: ", solution)
-
-# def steady_state_power(transition_matrix):
-#     power = 2
-#     while (True):
-#         transition_matrix = np.linalg.matrix_power(transition_matrix, power)
-#         elements_equal = True
-#         # We have to change this logic of element comparison of rows, help needed!!!
-#         for i in range(len(transition_matrix[0])):
-#             if abs(transition_matrix[0][i] - transition_matrix[1][i]) > 10 ** -5:
-#                 elements_equal = False
-#         print(power)
-#         print(transition_matrix[0])
-#         if elements_equal:
-#             break
-#         power = power + 1
-#
-#     return power,transition_matrix[0]
-#
-#
-# if __name__ == '__main__':
-#     print(steady_state_power(np.array([[0.9, 0.1, 0, 0], [0, 0, 0.8, 0.2], [0.5, 0.5, 0, 0], [0, 0, 0.1, 0.9]])))
