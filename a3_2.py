eabc44dd489287443795468614e4e6647f75906e
Maheshwari Gaganraj <maheshwarig22@gmail.com>
2020-03-03 02:17:35 -0500

    Assignment 3 Q2 part e and f

--- a3_2.py
+++ a3_2.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import math
 from random import seed
 from random import randint
 
@@ -80,6 +82,73 @@ def callPartC(times):
         l.append(t)
     partc(l)
 
+def parte(showGraph=True):
+    # Change the file path to q2.csv
+    df = pd.read_csv('C:/Users/gagan/Downloads/q2.csv', header=None);
+    df = df.values
+    df = df.flatten()
+    point, cdf = parta(df,False)
+    z = 1.96
+    upper_bound = []
+    lower_bound = []
+    n = len(point)
+    for val in cdf:
+        if val>1:
+            val = 1.0
+        upper_bound.append(val+z*(math.sqrt(val*(1-val)/n)))
+        lower_bound.append(val-z*(math.sqrt(val*(1-val)/n)))
+
+    X = point
+    Y = cdf
+    if showGraph:
+        plt.figure('eCDF', figsize=(10, 5))
+        plt.step(X, Y, label='eCDF')
+        plt.fill_between(X, lower_bound, upper_bound, color='royalblue', alpha=0.5,
+                         label='Normal Based 95% CI')
+        plt.scatter(X, [0] * len(X), color='red', marker='x', s=100, label='samples')
+        plt.xlabel('x')
+        plt.ylabel('Pr[X<=x]')
+        plt.title('eCDF with %d samples. Sample mean = %.2f.' % (n, np.mean(X)))
+        plt.legend(loc="upper left")
+        plt.grid()
+        plt.show()
+    return lower_bound,upper_bound
+
+def partf():
+    # Change the file path to q2.csv
+    df = pd.read_csv('C:/Users/gagan/Downloads/q2.csv', header=None);
+    df = df.values
+    df = df.flatten()
+    point, cdf = parta(df,False)
+    lower_bound,upper_bound = parte(showGraph=False)
+    dkw_upper = []
+    dkw_lower = []
+
+    n = len(point)
+    alpha = 0.05
+    beta = math.sqrt((math.log(2 / alpha)) / (2 * n))
+    for val in cdf:
+        if val>1:
+            val = 1.0
+        dkw_lower.append(val-beta)
+        dkw_upper.append(val+beta)
+    X = point
+    Y = cdf
+    plt.figure('eCDF', figsize=(10, 5))
+    plt.step(X, Y, color='yellow', label='F hat')
+    plt.fill_between(X, lower_bound, upper_bound, color='blue', alpha=0.5,
+                     label='Normal Based 95% CI')
+    plt.fill_between(X, dkw_lower, dkw_upper, color='grey', alpha=0.7,
+                     label='DKW Based 95% CI')
+
+    plt.scatter(X, [0] * len(X), color='red', marker='x', s=100, label='samples')
+    plt.xlabel('x')
+    plt.ylabel('Pr[X<=x]')
+    plt.title('eCDF with %d samples. Sample mean = %.2f.' % (n, np.mean(X)))
+    plt.legend(loc="upper left")
+    plt.grid()
+    plt.show()
+
 
 if __name__ == '__main__':
     # seed random number generator
@@ -90,3 +159,5 @@ if __name__ == '__main__':
     callPartC(10)
     callPartC(100)
     callPartC(1000)
+    parte()
+    partf()
