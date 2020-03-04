0940d469152f8f3bf0d4a7a23dbdfbdc5614dec2
Maheshwari Gaganraj <maheshwarig22@gmail.com>
2020-03-04 12:59:36 -0500

    Assignment 3 Q2 changes

--- a3_2.py
+++ a3_2.py
@@ -5,45 +5,53 @@ import math
 from random import seed
 from random import randint
 
+def drawplot(X,Y,S):
+    plt.figure('eCDF', figsize=(15, 10))
+    plt.step(X, Y, label='eCDF')
+    plt.xticks(np.arange(0,max(X)+1,1), rotation=90)
+    plt.scatter(S, [0] * len(S), color='red', marker='x', s=100, label='samples')
+    plt.xlabel('x')
+    plt.ylabel('Pr[X<=x]')
+    plt.title('eCDF')
+    plt.legend(loc="upper left")
+    plt.grid()
+    plt.show()
 
-def parta(inputList,showGraph=True):
-    n = len(inputList)
-    probabilities = dict()
-    inputList.sort()
-    for entry in inputList:
-        if entry not in probabilities:
-            probabilities[entry] = 1.0
-        else:
-            probabilities[entry] += 1.0
-    for key in probabilities.keys():
-        probabilities[key] = probabilities[key] / n
-    cdf = []
-    cumulative = 0
-    cdf.append(0)
-    for value in probabilities.values():
-        cumulative = cumulative + value
-        cdf.append(cumulative)
-    X = [0] + list(probabilities.keys())
-    Y = cdf
+# Code for question 2 part a
+def parta(S,showGraph=True):
+
+    # From https://www3.cs.stonybrook.edu/~anshul/courses/cse544_s20/eCDF.py
+    n = len(S)
+    Srt = sorted(S)
+    delta = 1
+    X = [min(Srt) - delta]
+    Y = [0]
+    for i in range(0, n):
+        X = X + [Srt[i], Srt[i]]
+        Y = Y + [Y[len(Y) - 1], Y[len(Y) - 1] + (1 / n)]
+    X = X + [max(Srt)]
+    Y = Y + [1]
+
+    # Insert a 0,0, since question asks for graph to start from zero
+    X.insert(0,0)
+    Y.insert(0,0)
+
     if showGraph:
-        plt.figure('eCDF', figsize=(10, 5))
-        plt.step(X, Y, label='eCDF')
-        plt.scatter(X, [0] * len(X), color='red', marker='x', s=100, label='samples')
-        plt.xlabel('x')
-        plt.ylabel('Pr[X<=x]')
-        plt.title('eCDF with %d samples. Sample mean = %.2f.' % (n, np.mean(X)))
-        plt.legend(loc="upper left")
-        plt.grid()
-        plt.show()
-    return X[1:], cdf[1:]
+        drawplot(X,Y,S)
+    # Returning from index one, since we inserted an extra entry of 0 to make our
+    # graph adhere to the requirements.
+    return X[1:], Y[1:]
 
+# Code for question 2 part c
 def partc(inputList):
     n = len(inputList)
     cdfList = np.zeros((n,99))
+    samplePoints = set()
     for i in range(0, n):
         point, cdf = parta(inputList[i],False)
 
         for j in range(len(point)):
+            samplePoints.add(point[j])
             cdfList[i][point[j]-1] = cdf[j]
 
         prev = 0
@@ -55,24 +63,16 @@ def partc(inputList):
 
     X = np.arange(0,99,1)
     Y = cdfList.mean(0)
-    plt.figure('eCDF', figsize=(10, 5))
-    plt.step(X, Y, label='eCDF')
-    plt.scatter(X, [0] * 99, color='red', marker='x', s=100, label='samples')
-    plt.xlabel('x')
-    plt.ylabel('Pr[X<=x]')
-    plt.title('eCDF with %d samples. Sample mean = %.2f.' % (n, np.mean(X)))
-    plt.legend(loc="upper left")
-    plt.grid()
-    plt.show()
+    drawplot(X,Y,list(samplePoints))
 
-
-
+# Code for question 2 part b
 def callPartA(times):
     input = []
     for _ in range(times):
         input.append(randint(1, 99))
     parta(input)
 
+# Code for question 2 part d
 def callPartC(times):
     l = list()
     for _ in range(times):
@@ -82,6 +82,7 @@ def callPartC(times):
         l.append(t)
     partc(l)
 
+# Code for question 2 part e
 def parte(showGraph=True):
     # Change the file path to q2.csv
     df = pd.read_csv('C:/Users/gagan/Downloads/q2.csv', header=None);
@@ -101,19 +102,21 @@ def parte(showGraph=True):
     X = point
     Y = cdf
     if showGraph:
-        plt.figure('eCDF', figsize=(10, 5))
-        plt.step(X, Y, label='eCDF')
-        plt.fill_between(X, lower_bound, upper_bound, color='royalblue', alpha=0.5,
-                         label='Normal Based 95% CI')
+        plt.figure('eCDF', figsize=(15, 10))
+        plt.step(X, Y, color='green', label='eCDF')
+        plt.step(X,lower_bound, color='blue', label='Normal Based 95% lower CI')
+        plt.step(X,upper_bound, color='blue', label='Normal Based 95% lower CI')
+        plt.xlim([0,2])
         plt.scatter(X, [0] * len(X), color='red', marker='x', s=100, label='samples')
         plt.xlabel('x')
         plt.ylabel('Pr[X<=x]')
-        plt.title('eCDF with %d samples. Sample mean = %.2f.' % (n, np.mean(X)))
+        plt.title('eCDF')
         plt.legend(loc="upper left")
         plt.grid()
         plt.show()
     return lower_bound,upper_bound
 
+# Code for question 2 part f
 def partf():
     # Change the file path to q2.csv
     df = pd.read_csv('C:/Users/gagan/Downloads/q2.csv', header=None);
@@ -134,17 +137,18 @@ def partf():
         dkw_upper.append(val+beta)
     X = point
     Y = cdf
-    plt.figure('eCDF', figsize=(10, 5))
+    plt.figure('eCDF', figsize=(15, 10))
     plt.step(X, Y, color='yellow', label='F hat')
-    plt.fill_between(X, lower_bound, upper_bound, color='blue', alpha=0.5,
-                     label='Normal Based 95% CI')
-    plt.fill_between(X, dkw_lower, dkw_upper, color='grey', alpha=0.7,
-                     label='DKW Based 95% CI')
+    plt.xlim([0, 2])
+    plt.step(X, lower_bound, color='blue', label='Normal Based 95% lower CI')
+    plt.step(X, upper_bound, color='blue', label='Normal Based 95% upper CI')
+    plt.step(X, dkw_lower, color='grey', label='DKW Based 95% lower CI')
+    plt.step(X, dkw_upper, color='grey', label='DKW Based 95% upper CI')
 
     plt.scatter(X, [0] * len(X), color='red', marker='x', s=100, label='samples')
     plt.xlabel('x')
     plt.ylabel('Pr[X<=x]')
-    plt.title('eCDF with %d samples. Sample mean = %.2f.' % (n, np.mean(X)))
+    plt.title('eCDF')
     plt.legend(loc="upper left")
     plt.grid()
     plt.show()
