8c4ba656ae6952546621a15c73e0ee371bb3b5d9
Maheshwari Gaganraj <maheshwarig22@gmail.com>
2020-04-28 05:32:00 -0500

    A6 Q5 b updated logic

--- Q5_b.py
+++ Q5_b.py
@@ -1,18 +1,23 @@
 import numpy as np
 import pandas as pd
 
+
+def MAP_decision(w, h_0):
+    sum_of_data = np.sum(w)
+    current = (sigma_2 * np.log(h_0/(1 - h_0))) / 2 * mu;
+    if sum_of_data >= current:
+        return 0
+    else:
+        return 1
+
+
 if __name__ == '__main__':
     X = pd.read_csv('q5.csv', header=None)
     p_s = [0.1, 0.3, 0.5, 0.8]
     mu = 0.5
     sigma_2 = 1.0
-    for index, row in X.iterrows():
-        for p in p_s:
-            prediction = []
-            for w in row:
-                current = (sigma_2*np.log((1-p)/p))/2*mu;
-                if w >= current:
-                    prediction.append(0)
-                else:
-                    prediction.append(1)
-            print("For P(H_0) = ", p, ", the hypotheses selected are :: ", prediction)
+    for p in p_s:
+        prediction = []
+        for (columnName, columnData) in X.iteritems():
+            prediction.append(MAP_decision(columnData, p))
+        print("For P(H_0) = ", p, ", the hypotheses selected are :: ", prediction)
